e04698dd9cf4c753df0ca259ecebd64883251813
Alex Dot dev <alexsz1256@gmail.com>
2023-07-23 20:37:16 +0100

    f

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,22 +2,22 @@
 module.exports = {
   content: ['./build/*.html',],
   theme: {
-    screens: {
-      'sm': '640px',
-      // => @media (max-width: 640px) { ... }
+    // screens: {
+    //   'sm': '640px',
+    //   // => @media (max-width: 640px) { ... }
 
-      'md': '768px',
-      // => @media (min-width: 768px) { ... }
+    //   'md': '768px',
+    //   // => @media (min-width: 768px) { ... }
 
-      'lg': '1024px',
-      // => @media (min-width: 1024px) { ... }
+    //   'lg': '1024px',
+    //   // => @media (min-width: 1024px) { ... }
 
-      'xl': '1280px',
-      // => @media (min-width: 1280px) { ... }
+    //   'xl': '1280px',
+    //   // => @media (min-width: 1280px) { ... }
 
-      '2xl': '1536px',
-      // => @media (min-width: 1536px) { ... }
-    },
+    //   '2xl': '1536px',
+    //   // => @media (min-width: 1536px) { ... }
+    // },
     extend: {
       'author' : {
         fontweight: 'bold',
